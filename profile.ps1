a67087568d14af186f51e0127cabe766916d67a9
Max Gavrilyuk <max.gavr.gmi@gmail.com>
2021-06-08 18:28:36 +0300

    Move Windows-specific files from 'dotfiles' repo

---
+++ profile.ps1
@@ -0,0 +1,10 @@
+﻿# run "PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force" to install posh-git
+# run "PowerShellGet\Update-Module posh-git" to update it
+Import-Module posh-git
+
+# tiny alias for git
+Set-Alias -Name g -Value git
+
+# set UTF-8 encoding
+$OutputEncoding = [Text.UTF8Encoding]::UTF8
+[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
