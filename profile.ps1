c8df7c6534f24697652c0561ba0cc920cb66263d
Max Gavrilyuk <max.gavr.gmi@gmail.com>
2021-06-18 17:17:14 +0300

    [powershell] Set alias for git before importing posh-git module

--- profile.ps1
+++ profile.ps1
@@ -1,9 +1,9 @@
-﻿# run "PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force" to install posh-git
+﻿# tiny alias for git
+Set-Alias -Name g -Value git
+
+# run "PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force" to install posh-git
 # run "PowerShellGet\Update-Module posh-git" to update it
 Import-Module posh-git
-
-# tiny alias for git
-Set-Alias -Name g -Value git
 
 # set UTF-8 encoding
 $OutputEncoding = [Text.UTF8Encoding]::UTF8
