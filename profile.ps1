b46e9a9de34bba70e9b2015040ba9a916cbb18a4
Maxim Gavrilyuk <maxim.gavrilyuk@matrix42.com>
2022-10-03 22:58:35 +0300

    [powershell] Enable autocomplete for dotnet CLI

--- profile.ps1
+++ profile.ps1
@@ -8,3 +8,12 @@ Import-Module posh-git
 # set UTF-8 encoding
 $OutputEncoding = [Text.UTF8Encoding]::UTF8
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+
+# PowerShell parameter completion shim for the dotnet CLI
+# https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
